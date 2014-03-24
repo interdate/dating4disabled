763d140086dda3e92f89543a8d61649749b46572
Pavel <pavel@interdate-ltd.co.il>
2014-03-24 15:28:50 +0200

    pavel marge

--- assets/backend/js/main.js
+++ assets/backend/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(
 	function(){
 		
-		$('.menu .item').tab();
+		$('.tabMenu .menu .item').tab();
 		
 		$('#getSideBar').click(function(){
 			//alert(444565464564);
@@ -9,8 +9,21 @@ $(document).ready(
 			  .sidebar({
 			    overlay: true
 			  })
-			  .sidebar('toggle')
+			  .sidebar('toggle')			  
 			;
+			
+			if($('.sidebar').hasClass('active')){
+				$('#getSideBar').addClass("move");
+				//$('#getSideBar').css({"margin-left":"250px"});
+			}
+			else{
+				$('#getSideBar').removeClass("move");
+			}
+			//$('.sidebar').sidebar('is closed', )
+			
+			
+			
+			
 		});
 		
 		
